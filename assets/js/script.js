20e7a52eddbd8cbc379d66f103212874db4832f1
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-08-17 22:09:57 -0400

    checking

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+
+$(".saveBtn").on("click", function() {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time,text);
+})
+
+function timeTracker() {
+    var timeNow = moment().hour();
+
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+
+        } 
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+
+        })
+
+} 
+
+$("#hour6 .description").val(localStorage.getItem("hour6"));
+$("#hour7 .description").val(localStorage.getItem("hour7"));
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour11"));
+$("#hour1 .description").val(localStorage.getItem("hour11"));
+$("#hour2 .description").val(localStorage.getItem("hour11"));
+
+
+timeTracker();
